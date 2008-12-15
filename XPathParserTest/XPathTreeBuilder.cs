4041e929e67413f607562fc11bcb4ffddc1ceab9
SergeyDubinets <SergeyDubinets@localhost>
2008-12-15 04:19:52 +0000

    Renamed CodeGalary to CodePlex

--- XPathParserTest/XPathTreeBuilder.cs
+++ XPathParserTest/XPathTreeBuilder.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
-using CodeGallery.XPathParser;
+using CodePlex.XPathParser;
 
 namespace XPathParserTest {
     class XPathTreeBuilder : IXPathBuilder<XElement> {
